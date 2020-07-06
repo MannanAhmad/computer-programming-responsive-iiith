e40fa2063c4ba283ce0b602ae8ac35bfb38a8136
Sayed Mannan Ahmad <mannansarsobiz@gmail.com>
2020-07-06 15:32:27 +0530

    Updated advControlFlow.js and set user input limit in between [0-20] for CSE3 lab

--- src/lab/exp3/simulation/advControlFlow.js
+++ src/lab/exp3/simulation/advControlFlow.js
@@ -167,18 +167,25 @@ window.view = {
     	this.clearDivs()
     	var selected_loop = this.getSelectedLoop()
 		var inputValue = document.getElementById('simpleLoopInput').value
-		if (selected_loop === 'for' && inputValue !== '' && !isNaN(model.inp) )
+		if (selected_loop === 'for' && (inputValue >=0 && inputValue <=20) && !isNaN(model.inp) )
 		{
 			this.displayLoop('forLoopContent', 'codeContentFor1')
 		}
-		if (selected_loop === 'while' && inputValue !== '' && !isNaN(model.inp))
+		else if (selected_loop === 'while' && (inputValue >=0 && inputValue <=20) && !isNaN(model.inp) )
 		{
 			this.displayLoop('whileLoopContent', 'codeContentWhile1')
+			
 		}
-		if (selected_loop === 'do-while' && inputValue !== '' && !isNaN(model.inp))
+		else if (selected_loop === 'do-while' && (inputValue >=0 && inputValue <=20) && !isNaN(model.inp) )
 		{
-		 	this.displayLoop('dowhileLoopContent', 'codeContentDoWhile1')
+			this.displayLoop('dowhileLoopContent', 'codeContentDoWhile1')
+			
 		}
+		else{
+			alert('"INVALID INPUT", Value must be in between [0-20]')
+			return false
+		}
+
 		this.disableButton('btnStart')
 		this.changeClass( 'btnStart', 'buttonDisable startButton')
 		this.enableButton('btnNext')
